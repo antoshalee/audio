19314fff8c228223b2ae25de0fdac18aed52bc1e
antoshalee <antoshalee@gmail.com>
2013-08-19 17:25:39 +0800

    added Order model

---
+++ app/models/order.rb
@@ -0,0 +1,4 @@
+class Order < ActiveRecord::Base
+  # belongs_to :speaker
+  # belongs_to :client
+end
---
+++ db/migrate/20130819092026_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :state
+      t.text :description
+      t.references :speaker, index: true
+      t.references :client, index: true
+      t.boolean :hidden
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
