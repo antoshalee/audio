a4d4ca50136400857884ce1de77454f47ce7ad1b
antoshalee <antoshalee@gmail.com>
2013-08-19 17:16:05 +0800

    init application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Audio::Application.config.secret_key_base = 'd5ab32c7d57922f7fd59f6dfdfed10c18e637a7c1be4c0f49e4e62f564d50160c8ca45c68a7a330ab42502747a7fcc51d72fa5f4138927c387ccbd389de21f32'
