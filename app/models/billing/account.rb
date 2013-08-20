1b399b0ffbfe90a5bae1ac2b48553655c46cc5ea
antoshalee <antoshalee@gmail.com>
2013-08-20 10:56:27 +0800

    added 'recalculate_balance' method to account

--- app/models/billing/account.rb
+++ app/models/billing/account.rb
@@ -1,5 +1,10 @@
 class Billing::Account < ActiveRecord::Base
   belongs_to :user
+  has_many :operations
 
   validates :user, presence: true
+
+  def recalculate_balance
+    self.balance = operations.reduce(0) {|sum, op| sum + op.value}
+  end
 end
--- spec/models/billing/account_spec.rb
+++ spec/models/billing/account_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Billing::Account do
-  describe "validations" do
-    let(:account) { FactoryGirl.build(:billing_account) }
+  let(:account) { FactoryGirl.build(:billing_account) }
 
+  describe "validations" do
     it "has a valid factory" do
       account.should be_valid
     end
@@ -11,6 +11,16 @@ describe Billing::Account do
     it "is not valid without user" do
       account.user = nil
       account.should_not be_valid
+    end
+  end
+
+  describe "#recalculate_balance" do
+    it "counts all operations and sets the balance attribute value" do
+      op1 = FactoryGirl.build(:billing_operation, value: -15, account: account)
+      op2 = FactoryGirl.build(:billing_operation, value: 20, account: account)
+      account.operations << op1 << op2
+      account.recalculate_balance
+      account.balance.should == 5
     end
   end
 end
