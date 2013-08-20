289649e1cb18856a4e8e9fbcf6affa5b9f1e9865
antoshalee <antoshalee@gmail.com>
2013-08-20 13:20:23 +0800

    use full names for variables

--- app/business/billing/transfer_manager.rb
+++ app/business/billing/transfer_manager.rb
@@ -1,14 +1,14 @@
 class Billing::TransferManager
-  attr_accessor :sender_acc, :recipient_acc, :value
+  attr_accessor :sender_account, :recipient_account, :value
 
   # Use mechanism of validations to check transfer possibility
   include ActiveModel::Validations
-  validates_presence_of :sender_acc, :recipient_acc, :value
+  validates_presence_of :sender_account, :recipient_account, :value
   validate :validate_sender_balance
 
-  def initialize sender_acc, recipient_acc, value
-    @sender_acc = sender_acc
-    @recipient_acc = recipient_acc
+  def initialize sender_account, recipient_account, value
+    @sender_account = sender_account
+    @recipient_account = recipient_account
     @value = value
   end
 
@@ -19,22 +19,22 @@ class Billing::TransferManager
   def transfer
     raise Billing::Errors::TransferIsNotPossible.new unless transfer_possible?
     ActiveRecord::Base.transaction do
-      transfer_obj = Billing::Transfer.create!(sender_account: @sender_acc,
-        recipient_account: @recipient_acc, value: @value)
+      transfer_obj = Billing::Transfer.create!(sender_account: @sender_account,
+        recipient_account: @recipient_account, value: @value)
       # create operations for sender and recipient
-      Billing::Operation.create!(account: @sender_acc, value: -@value, document: transfer_obj)
-      Billing::Operation.create!(account: @recipient_acc, value: @value, document: transfer_obj)
+      Billing::Operation.create!(account: @sender_account, value: -@value, document: transfer_obj)
+      Billing::Operation.create!(account: @recipient_account, value: @value, document: transfer_obj)
     end
   end
 
   private
 
   def validate_sender_balance
-    if @sender_acc && @sender_acc.balance < @value
+    if @sender_account && @sender_account.balance < @value
       errors.add(:base, "Недостаточно средств у отправителя для осуществления перевода")
     end
 
-    if @sender_acc == @recipient_acc
+    if @sender_account == @recipient_account
       errors.add(:base, "Нельзя переводить деньги самому себе")
     end
   end
