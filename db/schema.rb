c64c71a8f51c389c7be0f439c97841b65af6d9a2
antoshalee <antoshalee@gmail.com>
2013-10-06 22:14:08 +0400

    add properties to speakers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130820032310) do
+ActiveRecord::Schema.define(version: 20131006180451) do
 
   create_table "billing_accounts", force: true do |t|
     t.integer  "balance"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20130820032310) do
     t.datetime "updated_at"
   end
 
-  add_index "billing_accounts", ["user_id"], name: "index_billing_accounts_on_user_id"
+  add_index "billing_accounts", ["user_id"], name: "index_billing_accounts_on_user_id", using: :btree
 
   create_table "billing_operations", force: true do |t|
     t.integer  "account_id",    null: false
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 20130820032310) do
     t.datetime "updated_at"
   end
 
-  add_index "billing_operations", ["account_id"], name: "index_billing_operations_on_account_id"
-  add_index "billing_operations", ["document_id", "document_type"], name: "index_billing_operations_on_document_id_and_document_type"
+  add_index "billing_operations", ["account_id"], name: "index_billing_operations_on_account_id", using: :btree
+  add_index "billing_operations", ["document_id", "document_type"], name: "index_billing_operations_on_document_id_and_document_type", using: :btree
 
   create_table "billing_transfers", force: true do |t|
     t.integer  "value",                null: false
@@ -42,8 +42,8 @@ ActiveRecord::Schema.define(version: 20130820032310) do
     t.datetime "updated_at"
   end
 
-  add_index "billing_transfers", ["recipient_account_id"], name: "index_billing_transfers_on_recipient_account_id"
-  add_index "billing_transfers", ["sender_account_id"], name: "index_billing_transfers_on_sender_account_id"
+  add_index "billing_transfers", ["recipient_account_id"], name: "index_billing_transfers_on_recipient_account_id", using: :btree
+  add_index "billing_transfers", ["sender_account_id"], name: "index_billing_transfers_on_sender_account_id", using: :btree
 
   create_table "orders", force: true do |t|
     t.string   "state",       default: "created", null: false
@@ -56,17 +56,19 @@ ActiveRecord::Schema.define(version: 20130820032310) do
     t.datetime "updated_at"
   end
 
-  add_index "orders", ["client_id"], name: "index_orders_on_client_id"
-  add_index "orders", ["speaker_id"], name: "index_orders_on_speaker_id"
+  add_index "orders", ["client_id"], name: "index_orders_on_client_id", using: :btree
+  add_index "orders", ["speaker_id"], name: "index_orders_on_speaker_id", using: :btree
 
   create_table "speakers", force: true do |t|
     t.integer  "user_id"
     t.string   "account_number"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "sex",            null: false
+    t.integer  "timbre_level",   null: false
   end
 
-  add_index "speakers", ["user_id"], name: "index_speakers_on_user_id"
+  add_index "speakers", ["user_id"], name: "index_speakers_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -87,8 +89,8 @@ ActiveRecord::Schema.define(version: 20130820032310) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
