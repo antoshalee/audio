36f45c6f4c0abe8dadca4daef1f180ff4deb3329
antoshalee <antoshalee@gmail.com>
2013-12-09 23:11:00 +0400

    init registration form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007184256) do
+ActiveRecord::Schema.define(version: 20131209190032) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 20131007184256) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
+    t.string   "login"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
