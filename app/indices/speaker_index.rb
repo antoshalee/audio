2e4d1800598747f936d3f64eb1523323dad31548
Anton Lee <antoshalee@gmail.com>
2014-01-06 12:11:50 +0400

    temporarily comment index

--- app/indices/speaker_index.rb
+++ app/indices/speaker_index.rb
@@ -1,5 +1,5 @@
-ThinkingSphinx::Index.define :speaker, with: :active_record do
-  # indexes account_number
-  has voice_types(:name), as: :voice_type, facet: true
-  has voice_kind, facet: true
-end
+# ThinkingSphinx::Index.define :speaker, with: :active_record do
+#   # indexes account_number
+#   has voice_types(:name), as: :voice_type, facet: true
+#   has voice_kind, facet: true
+# end
