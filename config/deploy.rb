ef5596851189629eda82585bcad3f1f9b52095c5
Anton Lee <antoshalee@gmail.com>
2014-01-08 14:24:35 +0400

    deploy with unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,18 +16,27 @@ set :deploy_to, "/home/ubuntu/audio"
 set :user, "ubuntu"
 set :use_sudo, false
 
-
 set :ssh_options, { forward_agent: true, auth_methods: %w(publickey) }
+
+# === Unicorn ====
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+
 
 after "deploy:restart", "deploy:cleanup"
 
 require "bundler/capistrano"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+
+
+namespace :deploy do
+  task :restart do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  task :start do
+    run "bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
+  end
+  task :stop do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
+end
