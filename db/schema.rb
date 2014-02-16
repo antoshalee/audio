7222eb9ad9c6a63b00759846937b51c79700a36e
Anton Lee <antoshalee@gmail.com>
2014-02-16 12:48:53 +0400

    Add title to records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216073847) do
+ActiveRecord::Schema.define(version: 20140216083612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 20140216073847) do
     t.datetime "updated_at"
     t.integer  "owner_id",   null: false
     t.string   "owner_type", null: false
+    t.string   "title"
   end
 
   add_index "records", ["owner_id"], name: "index_records_on_owner_id", using: :btree
