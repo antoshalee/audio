cb8e018218c87424b9074da405a345a5cb6755fe
antoshalee <antoshalee@gmail.com>
2014-02-25 00:26:32 +0400

    Refresh online attribute logic

--- app/models/concerns/speaker/online_schedule.rb
+++ app/models/concerns/speaker/online_schedule.rb
@@ -10,7 +10,30 @@ class Speaker
       before_validation :reject_empty_online_days
     end
 
+    def refresh_online_attribute!
+      if should_be_online?
+        self.update_column(:online, true)
+      else
+        self.update_column(:online, false)
+      end
+    end
+
     private
+
+    def should_be_online?
+      now = Time.zone.now
+      range = online_schedule[current_dayname]
+      if range.present?
+        range = range.map { |t| Time.zone.parse t }
+        return true if now.between?(range[0], range[1])
+      end
+      false
+    end
+
+    # 'monday', 'tuesday' etc
+    def current_dayname
+      Time.zone.now.strftime("%A").downcase
+    end
 
     def reject_empty_online_days
       online_schedule.reject! { |_, range| range.all?(&:blank?) }
--- lib/tasks/online.rake
+++ lib/tasks/online.rake
@@ -2,5 +2,13 @@ namespace :online do
   desc "Refresh speakers' online attributes"
   task :refresh => :environment do
     puts "pending"
+
+    speakers = Speaker.
+      where("online_next_refresh < ? OR online_next_refresh IS NULL", Time.now)
+
+    speakers.find_each do |speaker|
+      speaker.refresh_online_attribute!
+    end
+
   end
 end
