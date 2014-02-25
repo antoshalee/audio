7a50c5f21e424e413e1ca2f72c5a9d76ced18a68
antoshalee <antoshalee@gmail.com>
2014-02-26 00:11:38 +0400

    Display real online status in list

--- app/decorators/speaker_decorator.rb
+++ app/decorators/speaker_decorator.rb
@@ -26,10 +26,26 @@ class SpeakerDecorator < Draper::Decorator
     downcase_and_join(age_types)
   end
 
+  def online_text
+    if online_schedule.key? 'daily'
+      "Ежедневно, #{online_timerange_text(online_schedule['daily'])}"
+    elsif online_schedule.key? 'weekdays'
+      "ПН-ПТ, #{online_timerange_text(online_schedule['weekdays'])}"
+    elsif online_schedule.key? 'weekend'
+      "СБ-ВС, #{online_timerange_text(online_schedule['weekend'])}"
+    else
+      online_schedule.keys.map { |d| I18n.t("short_weekdays.#{d}") }.join ", "
+    end
+  end
+
   private
 
   def downcase_and_join collection
     collection.map { |el| el.name.mb_chars.downcase }.join ", "
+  end
+
+  def online_timerange_text(range)
+    "#{range[0]}–#{range[1]}"
   end
 
 end
