fd573ab55356baadd6b651f4e2ffcc6a208401f1
Anton Lee <antoshalee@gmail.com>
2014-03-18 21:42:11 +0400

    Fix grammar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @speakers = Speaker.limit(10).decorate
-    @force_groups_to_be_opened = true
+    @force_groups_to_be_open = true
   end
 end
