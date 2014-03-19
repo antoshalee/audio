79c56ec0a9f083243090d99c4d08beac9b71a655
Anton Lee <antoshalee@gmail.com>
2014-03-19 22:54:52 +0400

    Add records validation

--- app/models/concerns/speaker/validations.rb
+++ app/models/concerns/speaker/validations.rb
@@ -5,6 +5,12 @@ class Speaker
     included do
       validates :sex, presence: true
       validates :rate, presence: true
+      validate :validate_records
     end
+
+    def validate_records
+      errors.add(:records, "отсутствуют") if records.size == 0
+    end
+
   end
 end
